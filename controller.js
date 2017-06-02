1cd763ea79efa1d8bb6660dbe753676f6c3addc9
Sydney Sprouse <sydneysprouse@Sydneys-MacBook-Air.local>
2017-06-01 19:24:39 -0600

    finished

--- controller.js
+++ controller.js
@@ -1,5 +1,10 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
+angular.module('userProfiles').controller('MainController', function($scope, myService) {
+
+    $scope.fetchBluths = function () {
+      $scope.bluths = myService.fetchData()
+    }
+
+    $scope.addBluth = function (newUser) {
+      $scope.bluths = myService.put(newUser)
+    }
 })
